c78c3e025172f7a415c947ac5ccaae5b9ed1e52b
Christian Karaschewitz <ckaraschewitz@gmail.com>
2018-02-03 14:00:23 +0100

    Add crypto calculation capabilities.

--- src/app/add-crypto-bar/add-crypto-bar.component.ts
+++ src/app/add-crypto-bar/add-crypto-bar.component.ts
@@ -12,16 +12,35 @@ export class AddCryptoBarComponent implements OnInit {
 crypto : Crypto = {
  id: 1,
  name: 'neo',
- amount: 0
+ amount: 0 
 }
+
 cryptos = CRYPTOS;
 
+cryptoValue : number = 0;
 
-  constructor() { }
+ onAddCrypto() {
+  this.cryptoValue = 1;
+}
+
+
+  constructor() {
+
+    
+   }
 
   ngOnInit() {
 
-  
-  }
+    var calculateTotal = (Price: number, Amount: number) : number => {
+      return Price * Amount; 
+    }
 
+      var Price : number = 42.851;
+      var Amount : number = 30;
+      console.log(Amount);
+      this.cryptoValue = calculateTotal(Price, Amount);
+      console.log(this.cryptoValue);
+
+
+}
 }
